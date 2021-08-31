f4de9b7785487a4d5bf2efcb42d8df0e20d353b8
Franklin Angelo Arenas <franklin.silva8@fatec.sp.gov.br>
2021-08-31 10:23:02 -0300

    criando views com views.generic

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.db import models
+from django.views.generic import DeleteView, ListView
+from django.views.generic.detail import DetailView
+
+from .models import Post
 
 # Create your views here.
+class PostListView(ListView):
+    model = Post
+
+class PostDetailView(DetailView):
+    model = Post
